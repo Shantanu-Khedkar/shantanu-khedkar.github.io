6666cce01be6b960c8212752e186a9b09c926ab5
Shantanu-Khedkar <shantanu.h.khedkar@protonmail.com>
2025-08-03 15:19:21 +0530

    add nav bar sticky positioning and flipping

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 function checkWindowSize() {
     if (window.innerWidth > 1024) {
         document.getElementById('top-nav').classList.remove('responsive-nav') // Close responsive nav
+        document.getElementById('top-nav').classList.remove('flipped')
     }
 }
 
 // Event listener for window resize
 window.addEventListener('resize', checkWindowSize);
+window.addEventListener('scroll', isOffscreen)
+
+
+function isOffscreen() {
+    const nav = document.getElementById('nav-opt')
+    const rect = nav.getBoundingClientRect();
+    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    if(rect.top <0 && nav.parentElement.classList.contains('flipped')){
+        console.log('flipped')
+        nav.parentElement.classList.remove('flipped')
+    }
+
+    if(rect.top+rect.height > viewportHeight && nav.parentElement.classList.contains('responsive-nav')){
+        console.log('flipped')
+        nav.parentElement.classList.add('flipped')
+    }
+
+    if(!nav.parentElement.classList.contains('responsive-nav') && nav.parentElement.classList.contains('flipped')){
+                console.log('flipped')
+        nav.parentElement.classList.remove('flipped')
+    }
+
+}
+
