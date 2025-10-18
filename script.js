c47956ffc41d1c3f8ee218f5894d8a88648dd25e
Shantanu-Khedkar <shantanu.h.khedkar@protonmail.com>
2025-10-18 16:53:57 +0530

    fix text, add nav btn underlines

--- script.js
+++ script.js
@@ -45,71 +45,23 @@ checkWindowSize()
 
 
 
-Array.from(document.getElementsByClassName("app-filebar-file")).forEach(function(elem){
-        elem.addEventListener("click", function(e){
-    if (!this.classList.contains("selected")){
-        Array.from(document.getElementsByClassName("app-filebar-file")).forEach(function(elem){
-            elem.classList.remove('selected')
-            document.getElementById("f-"+elem.innerText).classList.remove("visible")
-        })
-        this.classList.toggle("selected");
-        document.getElementById("f-"+this.innerText).classList.add("visible")
-    }
-})
+window.addEventListener('DOMContentLoaded', () => {
 
-})
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+                console.log(entry)
+                document.getElementById("n-"+id).classList.add('current')
+			} else {
+				document.getElementById("n-"+id).classList.remove('current')
+			}
+		});
+	});
 
-document.getElementById('index-input').value = document.getElementById("hero-editable").innerHTML
-document.getElementById('README-input').value = "# Text Editor Module\n\nThis is a simple text editor gimmick!\n\nWith the index.html file, you can see \nyour live edits to the hero section above...\nGo ahead, try it out. Feel free to go crazy\nwith your web dev skills. \n\nThe on the fly syntax highlighting is made\npossible in a manner similar to the clever code\n(here)[https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/] \nAnyways, continue exploring my site!\n"
-
-
-syncText(document.getElementById('index-input').value, document.getElementById('index-input').nextElementSibling)
-syncText(document.getElementById('README-input').value, document.getElementById('README-input').nextElementSibling)
-
-Array.from(document.getElementsByClassName("editarea-input")).forEach(function(elem){
-elem.addEventListener('input', function(){
-    syncScroll(this, this.nextElementSibling)
-    
-    syncText(this.value, this.nextElementSibling)
-    if (this.id == 'index-input'){
-    syncContent(this.value)
-    }
-})
-elem.addEventListener('scroll', function(){
-    syncScroll(this, this.nextElementSibling)
-    console.log(this.nextElementSibling.classList)
-    
-})
-})
-
-
-
-function syncScroll(element, editareaHighlight){
-    editareaHighlight.scrollTop = element.scrollTop;
-    editareaHighlight.scrollLeft = element.scrollLeft;
-}
-function syncText(text, editareaHighlight){
-      if(text[text.length-1] == "\n") { // If the last character is a newline character
-    text += " "; // Add a placeholder space character to the final line 
-  }
-  editareaHighlight.children[0].textContent = text
-  console.log(editareaHighlight.children[0])
-  Prism.highlightElement(editareaHighlight.children[0]);
-}
-function syncContent(html){
-document.getElementById("hero-editable").innerHTML = html
-}
-
-Array.from(document.getElementsByClassName("app-filebar-file")).forEach(function(elem){
-        elem.addEventListener("click", function(e){
-    if (!this.classList.contains("selected")){
-        Array.from(document.getElementsByClassName("app-filebar-file")).forEach(function(elem){
-            elem.classList.remove('selected')
-            document.getElementById("f-"+elem.innerText).classList.remove("visible")
-        })
-        this.classList.toggle("selected");
-        document.getElementById("f-"+this.innerText).classList.add("visible")
-    }
-})
-
-})
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+	
+});
