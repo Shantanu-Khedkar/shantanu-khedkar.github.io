727fd66d807e7368a1a7a1fd5faf0fe2172ed5b1
Shantanu-Khedkar <shantanu.h.khedkar@protonmail.com>
2025-12-21 19:48:24 +0530

    add toggle in responsive nav

--- script.js
+++ script.js
@@ -1,7 +1,86 @@
+var r = document.querySelector(':root');
+
+
+function extractVariableNames(line){
+    return line.split(" ")[0].slice(2, -1).split("_")
+}
+function packCSS(a, b){
+    return "--"+a+"_"+b
+}
+
+colorHandlerCSS = document.getElementById("colorHandlers").innerText.trim()
+colorHandlerCSS = colorHandlerCSS.slice(10,-2)
+colorHandlerCSS = colorHandlerCSS.split("\n")
+colorHandlerCSS = colorHandlerCSS.map(line => extractVariableNames(line));
+
+function checkDarkTheme() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 1 : 0;
+}
+function disableAllTransitions() {
+    const allElements = document.querySelectorAll('*');
+    allElements.forEach(element => {
+        element.style.transition = 'none';
+    });
+}
+function enableAllTransitions() {
+    const allElements = document.querySelectorAll('*');
+    allElements.forEach(element => {
+        element.style.transition = '';  // Reset the transition property
+    });
+}
+
+
+
+function switchTheme(theme){
+   disableAllTransitions();
+    colorHandlerCSS.forEach(function(pair){setProperty(packCSS(pair[0],pair[1]), pair[theme])})
+    document.querySelector(".logo").style.setProperty("background-image", " url('./assets/LogoThemed"+theme+".svg')")
+    document.getElementById("profile-vector").src = "./assets/profile"+theme+".svg"
+    
+    setTimeout(() => {
+    enableAllTransitions();
+}, 20);
+}
+function toggleTheme(){
+    switchTheme(globalTheme ? 0 : 1)
+    
+    globalTheme = globalTheme ? 0 : 1
+    document.getElementsByClassName('fa-'+themeIcon(globalTheme ? 0 : 1))[0].style.setProperty("color", "var(--light-grey-1_dark-grey-2)")
+    document.getElementsByClassName('fa-'+themeIcon(globalTheme ? 0 : 1))[1].style.setProperty("color", "var(--light-grey-1_dark-grey-2)")
+    document.getElementsByClassName('fa-'+themeIcon(globalTheme))[0].style.setProperty("color", "var(--primary)")
+    document.getElementsByClassName('fa-'+themeIcon(globalTheme))[1].style.setProperty("color", "var(--primary)")
+    
+}
+function setProperty(propertyName, propertyValue){
+    r.style.setProperty(propertyName, 'var(--'+propertyValue+')');
+}
+var themeIcon = (theme) => {return theme ? "moon":"sun"}
+globalTheme = checkDarkTheme()
+switchTheme(globalTheme)
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+    darkThemePreference = event.matches ? 1 : 0;
+    globalTheme = darkThemePreference
+    switchTheme(globalTheme)
+});
 function checkWindowSize() {
     if (window.innerWidth > 1024) {
         document.getElementById('top-nav').classList.remove('responsive-nav') // Close responsive nav
         document.getElementById('top-nav').classList.remove('flipped')
+        document.getElementById('menu-btn').firstElementChild.style.setProperty("display", "none")
+        document.getElementById('menu-btn').lastElementChild.style.setProperty("display", "block")
+        document.getElementById('menu-btn').children[1].style.setProperty("display", "block")
+        document.getElementById('menu-btn').style.setProperty("width", "96px")
+        document.getElementsByClassName('fa-'+themeIcon(globalTheme ? 0 : 1))[0].style.setProperty("color", "var(--light-grey-1_dark-grey-1)")
+        
+        document.getElementById('menu-btn').setAttribute('onclick', "toggleTheme()")
+    }   
+    else{
+        document.getElementById('menu-btn').firstElementChild.style.setProperty("display", "block")
+        document.getElementById('menu-btn').lastElementChild.style.setProperty("display", "none")
+        document.getElementById('menu-btn').children[1].style.setProperty("display", "none")
+        document.getElementById('menu-btn').style.setProperty("width", "48px")
+        document.getElementById('menu-btn').setAttribute('onclick',"this.parentElement.classList.toggle('responsive-nav'); isOffscreen()")
+        document.getElementsByClassName('fa-'+themeIcon(globalTheme ? 0 : 1))[1].style.setProperty("color", "var(--light-grey-1_dark-grey-1)")
     }
     if (window.innerHeight < 450){
         document.getElementById('nav-opt').parentElement.classList.add('covered')
@@ -88,53 +167,3 @@ document.getElementById("module-container").addEventListener("animationend", fun
     document.getElementById("module-container").style.webkitTransform = 'translate(0,0)'
 })
 
-var r = document.querySelector(':root');
-
-
-function extractVariableNames(line){
-    return line.split(" ")[0].slice(2, -1).split("_")
-}
-function packCSS(a, b){
-    return "--"+a+"_"+b
-}
-
-colorHandlerCSS = document.getElementById("colorHandlers").innerText.trim()
-colorHandlerCSS = colorHandlerCSS.slice(10,-2)
-colorHandlerCSS = colorHandlerCSS.split("\n")
-colorHandlerCSS = colorHandlerCSS.map(line => extractVariableNames(line));
-function checkDarkTheme() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 1 : 0;
-}
-function disableAllTransitions() {
-    const allElements = document.querySelectorAll('*');
-    allElements.forEach(element => {
-        element.style.transition = 'none';
-    });
-}
-function enableAllTransitions() {
-    const allElements = document.querySelectorAll('*');
-    allElements.forEach(element => {
-        element.style.transition = '';  // Reset the transition property
-    });
-}
-
-
-
-function switchTheme(theme){
-   disableAllTransitions();
-    colorHandlerCSS.forEach(function(pair){setProperty(packCSS(pair[0],pair[1]), pair[theme])})
-    document.querySelector(".logo").style.setProperty("background-image", " url('./assets/LogoThemed"+theme+".svg')")
-    document.getElementById("profile-vector").src = "./assets/profile"+theme+".svg"
-    setTimeout(() => {
-    enableAllTransitions();
-}, 20);
-}
-function setProperty(propertyName, propertyValue){
-    r.style.setProperty(propertyName, 'var(--'+propertyValue+')');
-}
-
-switchTheme(checkDarkTheme())
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-    darkThemePreference = event.matches ? 1 : 0;
-    switchTheme(darkThemePreference)
-});
